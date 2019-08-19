0a930486f2efd45be6d3052890d459152cb7c629
Kyle Tan <kyle@kyletan.com>
2019-08-19 11:00:15 -0700

    Add ping

--- app.py
+++ app.py
@@ -13,21 +13,26 @@ app.debug = False
 @app.route("/predict", methods=["POST"])
 def predict():
     input_string = request.form["message"]
-    prediction_number = clf.predict([input_string])[0]
-    prediction_string = prediction_map[prediction_number]
+    if input_string == "test":
+        prediction_string = ""
+    else:
+        prediction_number = clf.predict([input_string])[0]
+        prediction_string = prediction_map[prediction_number]
     return json.dumps({"Prediction": prediction_string})
+
+@app.route("/ping")
+def ping():
+    return "pong"
 
 if __name__ == "__main__":
     clf = TextClassifier(verbose=False)
-    clf.num_labels = 5
-    clf.output_model_file = "v2.h5"
+    clf.num_labels = 3
+    clf.output_model_file = "data/v2.h5"
     
     prediction_map = {
-            0: "workbot",
-            1: "crm",
-            2: "do_not_reply",
-            3: "knowledge_base",
-            4: "troubleshoot"
+            0: "do_not_reply",
+            1: "workbot",
+            2: "knowledge_base"
             }
 
     http_server = WSGIServer(("", settings.PORT), app)
